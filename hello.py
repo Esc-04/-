6831f0e36d3e0dff4d494f1fd693c439ddb5c5a7
Esc-04 <escho94@naver.com>
2025-01-10 01:13:56 +0900

    오류 수정작업

--- hello.py
+++ hello.py
@@ -2,11 +2,10 @@
 print('어서오세요, 국민우유집')
 print('딸기우유와 와플')
 print('초코우유와 고구마')
-print('hello ESC')
 
-<<<<<<< HEAD
+print('hello ESC')
+print('task2')
 print('task8')
 print('task9')
-=======
-print('task2')
->>>>>>> 142b2bf (task2 충돌 밑작업)
+
+
